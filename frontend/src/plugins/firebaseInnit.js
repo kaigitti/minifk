0128b20a82ad0cd6f6afaa112def644791857c89
kaigitti <k.kaigitti@gmail.com>
2020-06-20 22:07:17 +0700

    rm frontend, microsvc_ieie

--- frontend/src/plugins/firebaseInnit.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-import firebaseCon from './firebaseConfig'
-
-const firebaseApp = firebase.initializeApp(firebaseCon)
-
-export default firebaseApp.firestore()
--- frontend/yarnserve.cmd
+++
@@ -1,3 +0,0 @@
-yarn serve
-
-pause
